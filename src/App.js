94b82b695880e4ffc8942c411e067a94b256d608
kauishanshal <bhardwajkaushal443@gmail.com>
2023-02-24 13:50:14 +0530

    3rd commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
   return (
     <div className="App">
         <h1>App Component</h1>
+        <h2>This is added in the 2nd commit</h2>
+        <h2>This is working or not i am just check only</h2>
         {/* <Cls /> */}
         <Contuncont />
     </div>
